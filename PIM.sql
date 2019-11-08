6a89f8e362860b8bcd6c98a728e38875bd470381
Bringordie <48799015+Bringordie@users.noreply.github.com>
2019-11-08 16:14:06 +0100

    Update PIM.sql
    nameDescription added.
    Also test data for minorCategories and mainCategories added

--- PIM.sql
+++ PIM.sql
@@ -1,6 +1,6 @@
-DROP TABLE if exists mainCategories;
-DROP TABLE if exists minorCategories;
 DROP TABLE if exists products;
+DROP TABLE if exists minorCategories;
+DROP TABLE if exists mainCategories;
 
 
 CREATE TABLE mainCategories (
@@ -9,15 +9,30 @@ CREATE TABLE mainCategories (
     primary key (categoryid)
 );
 
+insert into mainCategories (mainCategoryName) values ("Frugt og grønt");
+insert into mainCategories (mainCategoryName) values ("Kød");
+insert into mainCategories (mainCategoryName) values ("Frost");
+insert into mainCategories (mainCategoryName) values ("Drikkevarer");
+
 CREATE TABLE minorCategories (
 	categoryid INTEGER not null AUTO_INCREMENT unique,
     minorCategoryName VARCHAR(45),
     primary key (categoryid)
 );
 
+insert into minorCategories (minorCategoryName) values ("Salat");
+insert into minorCategories (minorCategoryName) values ("Frugt");
+insert into minorCategories (minorCategoryName) values ("Oksekød");
+insert into minorCategories (minorCategoryName) values ("Svinekød");
+insert into minorCategories (minorCategoryName) values ("Grøntsager på frost");
+insert into minorCategories (minorCategoryName) values ("Is");
+insert into minorCategories (minorCategoryName) values ("Øl");
+insert into minorCategories (minorCategoryName) values ("Sodavand");
+
 CREATE TABLE products (
     producid INTEGER not null unique,
     name VARCHAR(45) not null,
+    nameDescription VARCHAR(45),
     description VARCHAR(200),
     companyName VARCHAR(45),
     price DOUBLE,
@@ -29,3 +44,4 @@ CREATE TABLE products (
     FOREIGN KEY (mainCategory) REFERENCES mainCategories(categoryid),
     FOREIGN KEY (minorCategory) REFERENCES minorCategories(categoryid)
 );
+
