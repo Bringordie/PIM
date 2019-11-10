e6f13d48b1fe128f070e3f3b0a5459530103fc92
Bringordie <48799015+Bringordie@users.noreply.github.com>
2019-11-10 18:31:17 +0100

    Fixed the exelsheet method so that it can now have a normal "start row" So that it now skips the first row as we didn't wanted to put them into the DB and they weren't the correct entities.

--- PIM/src/main/java/persistence/DBFacade.java
+++ PIM/src/main/java/persistence/DBFacade.java
@@ -11,11 +11,11 @@ import static persistence.DBConnection.getConnection;
 
 public class DBFacade implements Facade {
 
-    
     /**
      * @author Bringordie - Frederik Braagaard
-     * @param products - Takes an ArrayList of products which is then added to DB.
-     * 
+     * @param products - Takes an ArrayList of products which is then added to
+     * DB.
+     *
      */
     @Override
     public void reqisterProductsJson(ArrayList<Products> products) throws SQLException, ClassNotFoundException {
@@ -31,7 +31,7 @@ public class DBFacade implements Facade {
                 statement.setString(2, product.getName());
                 statement.setString(3, product.getDescription());
                 statement.setString(4, product.getNameDescription());
-                    statement.setString(5, product.getCompanyName());
+                statement.setString(5, product.getCompanyName());
                 statement.setDouble(6, product.getPrice());
                 statement.setInt(7, product.getQty());
                 statement.setString(8, product.getPictureName());
@@ -66,40 +66,47 @@ public class DBFacade implements Facade {
         String MinorCategory;
         String MainCategory;
 
-        for (Iterator iterator = dataHolder.iterator(); iterator.hasNext();) {
+        //for (Iterator iterator = dataHolder.iterator(); iterator.hasNext();) {
+        Iterator iterator = dataHolder.iterator();
+        if (iterator.hasNext()) {
+            iterator.next();
+        }
+
+        while (iterator.hasNext()) {
             List list = (List) iterator.next();
+            iterator.hasNext();
             ProductID = list.get(0).toString();
             ProductName = list.get(1).toString();
             //Might want to consider making an extra method to make it nicer.
-            if (list.get(2).toString()==""){
+            if (list.get(2).toString() == "") {
                 ProductNameDescription = null;
             } else {
-                ProductNameDescription = list.get(2).toString();    
+                ProductNameDescription = list.get(2).toString();
             }
             //Might want to consider making an extra method to make it nicer.
-            if (list.get(3).toString()==""){
+            if (list.get(3).toString() == "") {
                 ProductDescription = null;
             } else {
-                ProductDescription = list.get(3).toString();    
+                ProductDescription = list.get(3).toString();
             }
             //Might want to consider making an extra method to make it nicer.
-            if (list.get(4).toString()==""){
+            if (list.get(4).toString() == "") {
                 CompanyName = null;
             } else {
                 CompanyName = list.get(4).toString();
             }
             //Might want to consider making an extra method to make it nicer.
-            if (list.get(5).toString()==""){
+            if (list.get(5).toString() == "") {
                 Price = null;
             } else {
                 Price = list.get(5).toString();
             }
-            
+
             Quantity = list.get(6).toString();
             PictureName = list.get(7).toString();
             PublishedStatus = list.get(8).toString();
             MinorCategory = list.get(9).toString();
-            MainCategory = list.get(10).toString();            
+            MainCategory = list.get(10).toString();
 
             try {
                 String sql = "INSERT INTO products(productid, name, nameDescription, "
@@ -126,7 +133,6 @@ public class DBFacade implements Facade {
                 e.printStackTrace();
             }
         }
-        
-        
+
     }
 }
