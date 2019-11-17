db4c76e2e0e6449a620377ea8d33e9089a8030b4
Bringordie <48799015+Bringordie@users.noreply.github.com>
2019-11-17 18:08:45 +0100

    Update PIM.sql
    No longer need for inserts in the SQL script.

--- PIM.sql
+++ PIM.sql
@@ -10,25 +10,11 @@ CREATE TABLE mainCategories (
     primary key (categoryid)
 );
 
-insert into mainCategories (mainCategoryName) values ("Frugt og grønt");
-insert into mainCategories (mainCategoryName) values ("Kød");
-insert into mainCategories (mainCategoryName) values ("Frost");
-insert into mainCategories (mainCategoryName) values ("Drikkevarer");
-
 CREATE TABLE minorCategories (
 	categoryid INTEGER not null AUTO_INCREMENT unique,
     minorCategoryName VARCHAR(45),
     primary key (categoryid)
 );
-
-insert into minorCategories (minorCategoryName) values ("Salat");
-insert into minorCategories (minorCategoryName) values ("Frugt");
-insert into minorCategories (minorCategoryName) values ("Oksekød");
-insert into minorCategories (minorCategoryName) values ("Svinekød");
-insert into minorCategories (minorCategoryName) values ("Grøntsager på frost");
-insert into minorCategories (minorCategoryName) values ("Is");
-insert into minorCategories (minorCategoryName) values ("Øl");
-insert into minorCategories (minorCategoryName) values ("Sodavand");
 
 CREATE TABLE linkMinorMain (
 	mainid INT NOT NULL,
@@ -37,19 +23,11 @@ CREATE TABLE linkMinorMain (
     FOREIGN KEY (minorid) REFERENCES minorCategories(categoryid)
 );
 
-insert into linkMinorMain (mainid, minorid) values (1, 1);
-insert into linkMinorMain (mainid, minorid) values (1, 2);
-insert into linkMinorMain (mainid, minorid) values (2, 3);
-insert into linkMinorMain (mainid, minorid) values (2, 4);
-insert into linkMinorMain (mainid, minorid) values (3, 5);
-insert into linkMinorMain (mainid, minorid) values (3, 6);
-insert into linkMinorMain (mainid, minorid) values (4, 7);
-insert into linkMinorMain (mainid, minorid) values (4, 8);
 
 CREATE TABLE products (
     productid INTEGER not null unique,
     name VARCHAR(45) not null,
-    nameDescription VARCHAR(45),
+    nameDescription VARCHAR(80),
     description VARCHAR(2000),
     companyName VARCHAR(45),
     price DOUBLE,
