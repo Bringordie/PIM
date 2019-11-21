0e1d80d49cddaf7a4500d369cb3edbaad431f04e
Bringordie <48799015+Bringordie@users.noreply.github.com>
2019-11-21 11:00:09 +0100

    Added some logic tests and removed some setters in Categories

--- PIM/src/main/java/logic/Categories.java
+++ PIM/src/main/java/logic/Categories.java
@@ -23,16 +23,9 @@ public class Categories {
         return ID;
     }
 
-    public void setID(int ID) {
-        this.ID = ID;
-    }
 
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     @Override
