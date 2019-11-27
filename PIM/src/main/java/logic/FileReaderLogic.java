8e097dbe3a0d76023b4c84821172722976aa5b92
Bringordie <48799015+Bringordie@users.noreply.github.com>
2019-11-27 23:37:46 +0100

    Changed logic to read from a propertyfile.

--- PIM/src/main/java/logic/FileReaderLogic.java
+++ PIM/src/main/java/logic/FileReaderLogic.java
@@ -1,6 +1,9 @@
 package logic;
 
 import java.io.File;
+import java.io.InputStream;
+import java.util.Properties;
+import presentation.UploadFiles;
 
 /**
  *
@@ -9,12 +12,18 @@ import java.io.File;
 public class FileReaderLogic {
 
     public String FileCheck(String path) throws Exception {
-        String fullpath = "C:\\Users\\Frederik\\Desktop\\apache-tomcat-8.5.47\\webapps\\pictures\\"+path+".jpg";
+        //Loading property file
+        InputStream sa = UploadFiles.class.getResourceAsStream("/filepath.properties");
+        Properties properties = new Properties();
+        properties.load(sa);
+        // assign properties parameters
+        String filepath = properties.getProperty("picturepath");
+        String fullpath = filepath + path + ".jpg";
         String returnpath = "";
-        
+
         File file = new File(fullpath);
-        if(file.exists()) {
-            returnpath = "http://localhost:8080/pictures/"+path+".jpg";
+        if (file.exists()) {
+            returnpath = "http://localhost:8080/pictures/" + path + ".jpg";
         } else {
             returnpath = "http://localhost:8080/pictures/noimage.jpg";
         }
